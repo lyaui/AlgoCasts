b074a0d434dd93ff2fbe9d8d5d954c9d3b99a1af
Iris Huang <peiyu.huang@mile.cloud>
2024-05-13 11:51:28 +0800

    reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,10 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const absNum = Math.abs(n);
+  const reversedNum = parseInt((n + '').split('').reverse().join(''));
+  return n >= 0 ? reversedNum : -reversedNum;
+}
 
 module.exports = reverseInt;
