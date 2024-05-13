3d2f8c44acdda624c2ceb78bed857bafb98adbb0
Iris Huang <peiyu.huang@mile.cloud>
2024-05-13 12:04:15 +0800

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const counter = {};
+  for (const _char of str) {
+    counter[_char] = counter[_char] === undefined ? 0 : counter[_char] + 1;
+  }
+  let max = -Infinity;
+  let result;
+  for (const _char in counter) {
+    const val = counter[_char];
+    if (val > max) {
+      max = val;
+      result = _char;
+    }
+  }
+
+  return result;
+}
 
 module.exports = maxChar;
